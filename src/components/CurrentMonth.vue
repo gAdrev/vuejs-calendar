975b98799f80325fc5e38b4907d3e74619ea9457
Javier Villa <javiervilla00@gmail.com>
2017-08-20 17:13:02 +0200

    Component to display the current month (lec 140)

---
+++ src/components/CurrentMonth.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>{{ formattedDate }}</div>
+</template>
+<script>
+export default {
+    computed: {
+        month() {
+            return this.$store.state.currentMonth;
+        },
+        year() {
+            return this.$store.state.currentYear;
+        },
+        formattedDate() {
+            return this.$moment(`${this.year}-${this.month}-1`, "yyyy-M-D")
+                .format("MMMM YYYY");
+        }
+    }
+}
+</script>
+
